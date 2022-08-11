f14df9c13e48f864af65dd0f0076254ed14808ff
KozlovAleksandr <aleksandr.an.kozlov@gmail.com>
2022-08-11 19:05:54 +0300

    fix

--- homework/timer.js
+++ homework/timer.js
@@ -1,19 +1,19 @@
-require('moment-precise-range-plugin');
-const moment = require('moment');
-const EventEmitter = require('events')
-const [ dateStringInFuture ] = process.argv.slice(2);
-const DATE_FORMAT_PATTERN = 'YYYY-MM-DD HH:mm:ss'
+require("moment-precise-range-plugin");
+const moment = require("moment");
+const EventEmitter = require("events");
+const [dateStringInFuture] = process.argv.slice(2);
+const DATE_FORMAT_PATTERN = "YYYY-MM-DD HH:mm:ss";
 
 const getDate = (dateString) => {
-  const [hour, day, month, year] = dateString.split('-');
+  const [hour, day, month, year] = dateString.split("-");
   return new Date(Date.UTC(year, month - 1, day, hour));
-}
+};
 
 const showRemainingTime = (requestedDate) => {
   const now = newDate();
 
   if (now >= requestedDate) {
-    emitter.emit('timerEnd');
+    emitter.emit("timerEnd");
   } else {
     const curDateFormatted = moment(now, DATE_FORMAT_PATTERN);
     const reqDateFormatted = moment(now, DATE_FORMAT_PATTERN);
@@ -26,17 +26,16 @@ const showRemainingTime = (requestedDate) => {
 
 const showFinish = (timerId) => {
   clearImmediate(timerId);
-  console.log('Время пришло');
+  console.log("Время пришло");
 };
 
 const emitter = new EventEmitter();
 const requestedDate = getDate(dateStringInFuture);
 const timerId = setInterval(() => {
-  emitter.emit('timerTick', requestedDate);
-}, 1000)
-  
-  
-  emitter.on('timerTick', showRemainingTime)
-  emitter.on('timerEnd', () => {
-    showFinish(timerId)
-  })
+  emitter.emit("timerTick", requestedDate);
+}, 1000);
+
+emitter.on("timerTick", showRemainingTime);
+emitter.on("timerEnd", () => {
+  showFinish(timerId);
+});
